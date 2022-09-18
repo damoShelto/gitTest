e113302e62ee9a9620933592d97e0e16ae8d17d3
damoShelto <52026046+damoShelto@users.noreply.github.com>
2022-09-18 16:01:17 +0800

    Add files via upload

---
+++ include/gitTest.h
@@ -0,0 +1,21 @@
+
+#ifndef     __GIT_TEST_H
+#define     __GIT_TEST_H
+//------------------------------
+
+#include <Arduino.h>
+
+
+class gitTest{
+
+
+
+
+public: 
+    String getVer(void);
+
+};
+
+
+//------------------------------
+#endif
